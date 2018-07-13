e90a9b0a64c13761517d21aca06aac2f6f7ac338
adenapila <adenapila@gmail.com>
2018-07-13 16:23:09 +0700

    V 1.0

---
+++ laravel/app/Providers/AppServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (!defined('ADMIN')) {
+           define('ADMIN', config('variables.APP_ADMIN', 'admin'));
+        }
+        require_once base_path('resources/macros/form.php');
+        Schema::defaultStringLength(191);
+
+        $this->app->bind('path.public', function() {
+            return base_path().'/../public_html';
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
